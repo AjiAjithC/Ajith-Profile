498bb84f3a4639130f848859eabc8192f8cdea24
Ajith <102502422+Ajith-perisync@users.noreply.github.com>
2025-02-28 10:01:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,11 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { HashRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
-  </HashRouter>
-);
+  </HashRouter>,
+)
