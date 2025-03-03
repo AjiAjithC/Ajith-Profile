6bfe9da045d18393ded79711c67714525bca4d0b
Ajith-perisync <ajith@perisync.com>
2025-03-03 21:27:23 +0530

    new

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: './Ajith-Profile',
   plugins: [react(),tailwindcss()],
-  base: '/Ajith-Profile/',
-  resolve: {
-    extensions: ['.js', '.jsx'], // 👈 Ensure Vite resolves .jsx correctly
-  },
-  server: {
-    mimeTypes: {
-      'text/jsx': ['js', 'jsx'], // 👈 Force Vite to serve JSX as JavaScript
-    },
-  },
 })
   
